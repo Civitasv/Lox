03ff8b718ccccf65273afc896ac30a9997768f6d
Civitasv <hscivitasv@gmail.com>
2022-04-29 15:32:05 +0800

    lox add statements and state

--- src/main/java/com/civitasv/lox/Expr.java
+++ src/main/java/com/civitasv/lox/Expr.java
@@ -3,22 +3,9 @@ package com.civitasv.lox;
 import java.util.List;
 
 abstract class Expr {
-
-    // Visitor pattern, with this, we don't need
-    // to write every method in parent class and
-    // then override them in subclass. Instead, we
-    // only supply a `accept` method in parent class
-    // and override it in subclass. In this way, when
-    // we want to add method for each subclass, we
-    // declare a class which implement this `Visitor`
-    // interface and then implements all the methods
-    // in it, in `visitBinaryExpr` method, we can write
-    // logic to handle `Binary` expression, in
-    // `visitGroupingExpr` method, we can write
-    // different logic to handle `Grouping` expression, etc.
-    // Thus, we can only write the function once without
-    // overriding by all subclasses.
     interface Visitor<R> {
+        R visitAssignExpr(Assign expr);
+
         R visitBinaryExpr(Binary expr);
 
         R visitGroupingExpr(Grouping expr);
@@ -26,6 +13,23 @@ abstract class Expr {
         R visitLiteralExpr(Literal expr);
 
         R visitUnaryExpr(Unary expr);
+
+        R visitVariableExpr(Variable expr);
+    }
+
+    static class Assign extends Expr {
+        Assign(Token name, Expr value) {
+            this.name = name;
+            this.value = value;
+        }
+
+        @Override
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitAssignExpr(this);
+        }
+
+        final Token name;
+        final Expr value;
     }
 
     static class Binary extends Expr {
@@ -84,6 +88,19 @@ abstract class Expr {
 
         final Token operator;
         final Expr right;
+    }
+
+    static class Variable extends Expr {
+        Variable(Token name) {
+            this.name = name;
+        }
+
+        @Override
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitVariableExpr(this);
+        }
+
+        final Token name;
     }
 
     abstract <R> R accept(Visitor<R> visitor);
